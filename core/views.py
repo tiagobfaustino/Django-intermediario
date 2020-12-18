c51964cd7d8ce7ad37a6b4fdfa4917b3dfe94543
tiagobfaustino <tiagobfaustino@gmail.com>
2020-12-17 21:47:23 -0300

    Salvando dado no banco de dados mysql

--- core/views.py
+++ core/views.py
@@ -41,12 +41,13 @@ def produto(request):
     if request.method == 'POST':
         form = ProdutoModelForm(request.POST, request.FILES)
         if form.is_valid():
-            prod = form.save(commit=False)
 
+            form.save()
+            '''prod = form.save(commit=False)
             print(f'Nome: {prod.nome}')
             print(f'Preço: {prod.preco}')
             print(f'Estoque: {prod.estoque}')
-            print(f'Imagem: {prod.imagem}')
+            print(f'Imagem: {prod.imagem}')'''
 
             messages.success(request, 'Produto salvo com sucesso.')
             form = ProdutoModelForm()
